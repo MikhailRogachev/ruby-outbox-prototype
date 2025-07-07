b9cc479834f833b058b5a1081acc88da1a5eb7f1
Mikhail Rogachev <Mikhail.rogachev@gmail.com>
2025-07-07 14:49:36 +0200

    update customer model

--- src/ruby-outbox-core/Models/Customer.cs
+++ src/ruby-outbox-core/Models/Customer.cs
@@ -1,15 +1,7 @@
 ﻿using ruby_outbox_core.Contracts.Enums;
 
 namespace ruby_outbox_core.Models;
-/*{
-  "MySecret": "azure-subscription-17bdc332-0a80-450f-b4b8-b0300c42498e",
-  "KeyVaultConfig:KeyVaultName": "pvad-dev-weu-app-kv",
-  "AzureConfig:TenantId": "9993c174-ed13-464f-8384-2e28f515ff6a",
-  "AzureConfig:ClientSecret": "1hP8Q~uApXi7xaHj-7m6XvTbP9eetVA_TpBE8bCY",
-  "AzureConfig:ClientId": "ccd16d25-d15e-4308-a7a5-5faa68f01010",
-  "MyRegion": "westeurope",
-  "ResourceGroup": "mikhail-rogachev-vms-rg"
-}*/
+
 public class Customer : Base
 {
     public virtual ICollection<Vm> Vms { get; set; } = new List<Vm>();
